d192ce98d6b5f8779a2fa8b40a27669cf122f230
Shengjie0266 <105021040@live.asia.edu.tw>
2016-12-26 15:58:05 +0800

    ex

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -4,14 +4,44 @@
  例如: 9122 的各位數字和為 9+1+2+2=14、3128 的各位數字和為 3+1+2+8=14 而 5112 的各位數字和為 5+1+1+2=9。
  所以輸入 9122 3128 5112 需輸出 5112 3128 9122 ，這是因為 5112(9) < 3128(14) < 9122(14)，其中又因為 3128 與 9122 兩者的各位數字和都是 14，所以將 數值小的 3128 放前面。則經由計算可得其識別碼為 165 ，乃是 11 之倍數，故此為一合法之 ISBN 碼。輸入一串 ISBN 碼，以空格隔開。
  * Date: 2016/12/26
- * Author: 1050210XX 周永振老師
+ * Author: 105021040 洪聖傑
  */
+
+import java.util.Scanner;
 
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int [] data=new int[n];
+		int [] a =new int[n];
+		for(int i=0;i<n;i++){
+			data[i]=scn.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			while(data[i]>0){
+				a[i]=a[i]+data[i]%10;
+				data[i]=data[i]/10;
+			}
+		}
+		for(int i=0;i<n;i++){
+			System.out.print(data[i]+"\t");
+			for(int j=0;j<n;j++){
+				if(a[i]>a[j]){
+					int b=data[i];
+					data[i]=data[j];
+					data[i]=b;
+				}else if(a[i]==a[j]){
+					if(data[i]<data[j]){
+						int c=data[i];
+						data[i]=data[j];
+						data[j]=c;
+					}
+				}
+			}
+			System.out.print(data[i]+"\t");
+		}
 	}
-
 }
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,14 +2,19 @@
 /*
  * Topic: 給定一字串，請將此字串中，每個字元的 ASCII 編碼數值進行加總，輸出其編碼數 值總和。舉例來說，若輸入之字串為 “abcdefghij” 共 10 個字元，其 ASCII 編碼依序為 “97 98 99 100 101 102 103 104 105 106” ，則編碼總和輸出為 1015 。
  * Date: 2016/12/26
- * Author: 1050210XX 周永振老師
+ * Author: 105021040 洪聖傑
  */
-
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		String str=scn.next();
+		int i[]=new int[999];
+		for(int a=0;a<str.length();a++){
+			
+		}
 	}
 
 }
